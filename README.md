1c7decd2d40b6a94f1f5eba6a905c4cd97ce4a84
SpaceLudens <kurmanrm@gmail.com>
2024-11-17 19:22:02 +0500

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SpaceLudens/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SpaceLudens/java-project-99/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/b7db3ed0f57ec4393936/maintainability)](https://codeclimate.com/github/SpaceLudens/java-project-99/maintainability)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
+    id("jacoco")
+    id("checkstyle")
 }
 
 group = "hexlet.code"
@@ -28,8 +30,25 @@ dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+jacoco {
+    toolVersion = "0.8.8" // Версия JaCoCo
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // Генерация отчета после выполнения тестов
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // Убедитесь, что тесты выполнены перед созданием отчета
+    reports {
+        html.required = true
+        xml.required = true
+        csv.required = false
+    }
 }
